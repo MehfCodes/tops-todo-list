32da57b10853252022f2080ec37e130472ec3e48
MehfCodes <mrmohammadehf@gmail.com>
2021-07-28 02:09:07 +0430

    cache strategy was implemented

---
+++ util/cache.ts
@@ -0,0 +1,17 @@
+import { clientRedis, getCache } from './../server';
+
+export async function cacheOrSet(key: string, cb) {
+  try {
+    const inCache = await getCache(key);
+    if (inCache) {
+      return JSON.parse(inCache);
+    } else {
+      const data = await cb();
+      clientRedis.setex('tasks', 3600, JSON.stringify(data));
+      return data;
+    }
+  } catch (error) {
+    console.log(error);
+    return error.message;
+  }
+}
